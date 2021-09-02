c665d09e59221a92dd74c42b47bcafd74a8c45fe
caffi <caffi.senna@gmail.com>
2021-09-02 10:58:46 +0900

    Eラン修正

--- resources/views/elearnings/table.blade.php
+++ resources/views/elearnings/table.blade.php
@@ -2,25 +2,18 @@
     <table class="table" id="elearnings-table">
         <thead>
             <tr>
-                <th>Q1</th>
-                <th colspan="3">Action</th>
+                <th colspan="3">アクション</th>
             </tr>
         </thead>
         <tbody>
         {{-- @foreach($elearnings as $elearning) --}}
             <tr>
-                <td>{{ $elearning->q1 }}</td>
                 <td width="120">
                     @if($elearning->id)
+                    <p class="text-success">Eラーニングを受講済みです</p>
                     {!! Form::open(['route' => ['elearnings.destroy', $elearning->id], 'method' => 'delete']) !!}
                     <div class='btn-group'>
-                        <a href="{{ route('elearnings.show', [$elearning->id]) }}" class='btn btn-default btn-xs'>
-                            <i class="far fa-eye"></i>
-                        </a>
-                        <a href="{{ route('elearnings.edit', [$elearning->id]) }}" class='btn btn-default btn-xs'>
-                            <i class="far fa-edit"></i>
-                        </a>
-                        {!! Form::button('<i class="far fa-trash-alt"></i>', ['type' => 'submit', 'class' => 'btn btn-danger btn-xs', 'onclick' => "return confirm('Are you sure?')"]) !!}
+                        {!! Form::button('<i class="far fa-trash-alt"></i>受講歴を削除する', ['type' => 'submit', 'class' => 'btn btn-danger btn-lg', 'onclick' => "return confirm('本当に削除しますか?')"]) !!}
                     </div>
                     {!! Form::close() !!}
                     @endif
