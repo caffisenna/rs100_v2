6526505d383d525efb8f6b3ef87b8b783702f82e
caffi <caffi.senna@gmail.com>
2021-10-03 21:10:09 +0900

    Squashed commit of the following:
    commit f0f313823baf9b2779a3bfa615b372d9c1009a7d
    Author: caffi <caffi.senna@gmail.com>
    Date:   Sun Oct 3 21:09:58 2021 +0900
    
        Update welcome.blade.php
    
    commit e8f6937635b1a237bf1d9a3ef869e9eb6df4fe87
    Author: caffi <caffi.senna@gmail.com>
    Date:   Sat Oct 2 21:30:19 2021 +0900
    
        Update app.blade.php
    
    commit 82aba0e621b2dd646133579e23b95bfd961f59ea
    Author: caffi <caffi.senna@gmail.com>
    Date:   Fri Oct 1 22:39:35 2021 +0900
    
        Update slack.php
    
    commit c848e2bd4c9db7f8721da5c86e1120ff4cb18907
    Author: caffi <caffi.senna@gmail.com>
    Date:   Fri Oct 1 22:24:08 2021 +0900
    
        update adminconfig

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -459,12 +459,12 @@
 
                     <div class="p-6 border-t border-gray-200 dark:border-gray-700">
                         <div class="flex items-center">
-                            <span uk-icon="icon:question; ratio:2"></span>
+                            {{-- <span uk-icon="icon:question; ratio:2"></span>
                             <div class="ml-4 text-lg leading-7 font-semibold">
                                 <a href="#" class="underline text-gray-900 dark:text-white">
                                     よくある質問
                                 </a>
-                            </div>
+                            </div> --}}
                         </div>
                     </div>
 
@@ -494,7 +494,7 @@
                     <div class="flex items-center">
 
 
-                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                        {{-- <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                             stroke-width="2" viewBox="0 0 24 24" class="ml-4 -mt-px w-5 h-5 text-gray-400">
                             <path
                                 d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z">
@@ -503,7 +503,7 @@
 
                         <a href="" class="ml-1 underline">
                             スポンサー
-                        </a>
+                        </a> --}}
                     </div>
                 </div>
 
