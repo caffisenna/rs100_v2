469bf037cf60d4d4ab12aeadbaf0bda162f5366b
caffi <caffi.senna@gmail.com>
2021-10-12 17:03:22 +0900

    Squashed commit of the following:
    commit 579a5d466b5528d468469359e4e01918b0e7a9d7
    Author: caffi <caffi.senna@gmail.com>
    Date:   Tue Oct 12 17:03:10 2021 +0900
    
        include UIkit
    
    commit 65925c12a8820b1cfb9d9b2b7f2b794082edbe0b
    Author: caffi <caffi.senna@gmail.com>
    Date:   Wed Oct 6 22:21:01 2021 +0900
    
        Update welcome.blade.php
    
    commit 7705ff561e11b16241547baefa00977f170a1d3f
    Author: caffi <caffi.senna@gmail.com>
    Date:   Wed Oct 6 00:34:39 2021 +0900
    
        Update index.blade.php
    
    commit 0cd1250047c708f01d3ba473ec284793993b08ac
    Author: caffi <caffi.senna@gmail.com>
    Date:   Tue Oct 5 13:45:18 2021 +0900
    
        Update welcome.blade.php
    
    commit 39b983c5d883f361513ebf575750fc62ed22d2ab
    Author: caffi <caffi.senna@gmail.com>
    Date:   Tue Oct 5 13:36:56 2021 +0900
    
        Update welcome.blade.php
    
    commit 13f10069f6e6a9310cc3c20318ae2956279638a3
    Author: caffi <caffi.senna@gmail.com>
    Date:   Tue Oct 5 13:24:14 2021 +0900
    
        Update welcome.blade.php
    
    commit f0f313823baf9b2779a3bfa615b372d9c1009a7d
    Author: caffi <caffi.senna@gmail.com>
    Date:   Sun Oct 3 21:09:58 2021 +0900
    
        Update welcome.blade.php
    
    commit e8f6937635b1a237bf1d9a3ef869e9eb6df4fe87
    Author: caffi <caffi.senna@gmail.com>
    Date:   Sat Oct 2 21:30:19 2021 +0900
    
        Update app.blade.php
    
    commit 82aba0e621b2dd646133579e23b95bfd961f59ea
    Author: caffi <caffi.senna@gmail.com>
    Date:   Fri Oct 1 22:39:35 2021 +0900
    
        Update slack.php
    
    commit c848e2bd4c9db7f8721da5c86e1120ff4cb18907
    Author: caffi <caffi.senna@gmail.com>
    Date:   Fri Oct 1 22:24:08 2021 +0900
    
        update adminconfig

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -390,11 +390,11 @@
     </style>
 
     <!-- UIkit CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.7.2/dist/css/uikit.min.css" />
+    <link rel="stylesheet" href="{{ url('/css/uikit.min.css') }}" />
 
     <!-- UIkit JS -->
-    <script src="https://cdn.jsdelivr.net/npm/uikit@3.7.2/dist/js/uikit.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/uikit@3.7.2/dist/js/uikit-icons.min.js"></script>
+    <script src="{{ url('/') }}js/uikit.min.js"></script>
+    <script src="{{ url('/') }}js/uikit-icons.min.js"></script>
 </head>
 
 <body class="antialiased">
