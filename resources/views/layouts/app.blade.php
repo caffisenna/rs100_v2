7d308ac27d0600f8c4cb73b3ca5b02e1884bcbc6
caffi <caffi.senna@gmail.com>
2021-10-30 00:35:04 +0900

    Squashed commit of the following:
    commit 98cb8bb61cc284af52fbebf9c5e6a35be814f3aa
    Author: caffi <caffi.senna@gmail.com>
    Date:   Sat Oct 30 00:34:51 2021 +0900
    
        Update app.blade.php
    
    commit f312f3cd5307dce7f747f2397ea39351c995f6d5
    Author: caffi <caffi.senna@gmail.com>
    Date:   Sat Oct 30 00:31:46 2021 +0900
    
        Update welcome.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,8 +59,6 @@
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item dropdown user-menu">
                     <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">
-                        {{-- <img src="{{ url('/images/logo_color.png') }}"
-                         class="user-image img-circle elevation-2" alt="User Image"> --}}
                         <span class="d-none d-md-inline">{{ Auth::user()->name }}</span>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
