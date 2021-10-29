0f41fe67b21fecad483c6757ac85b3c75180e72e
caffi <caffi.senna@gmail.com>
2021-10-30 00:31:55 +0900

    Squashed commit of the following:
    commit f312f3cd5307dce7f747f2397ea39351c995f6d5
    Author: caffi <caffi.senna@gmail.com>
    Date:   Sat Oct 30 00:31:46 2021 +0900
    
        Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>東京連盟RS100kmハイクエントリーシステム</title>
+    <title>{{ config('app.name') }}</title>
 
     <!-- Styles -->
     <style>
