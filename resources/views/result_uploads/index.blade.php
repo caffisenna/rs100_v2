bd1d90c0305d5bf5684fa95a36cb8afc86920b1a
caffi <caffi.senna@gmail.com>
2021-11-06 20:45:23 +0900

    Squashed commit of the following:
    commit a3e17cf735a44da56d1f071236d9da1777b84400
    Author: caffi <caffi.senna@gmail.com>
    Date:   Sat Nov 6 20:45:14 2021 +0900
    
        Update index.blade.php
    
    commit 5de0b7596d6c155290d01c1e5bd7e764db7c1e5d
    Author: caffi <caffi.senna@gmail.com>
    Date:   Sat Nov 6 19:17:02 2021 +0900
    
        Update table.blade.php
    
    commit e58452bf13b2a72c0a3ee9836f05b8a53ec798b7
    Author: caffi <caffi.senna@gmail.com>
    Date:   Sat Nov 6 14:12:17 2021 +0900
    
        Update table.blade.php
    
    commit 62c194306045ddea8f548fd28b01afeece3ad341
    Author: caffi <caffi.senna@gmail.com>
    Date:   Fri Nov 5 22:59:05 2021 +0900
    
        Update planUploadController.php
    
    commit 10d3c708051bef6b2493de6a9276db04d1397a72
    Author: caffi <caffi.senna@gmail.com>
    Date:   Fri Nov 5 22:40:36 2021 +0900
    
        強制送還
    
    commit 964dfcaa76e8ba03f9c0871bf50da87869ad41fb
    Author: caffi <caffi.senna@gmail.com>
    Date:   Fri Nov 5 22:13:04 2021 +0900
    
        Update planUploadController.php
    
    commit a33c58063e62c0fbe33a98738b18594861cbd276
    Author: caffi <caffi.senna@gmail.com>
    Date:   Fri Nov 5 22:13:01 2021 +0900
    
        Update table.blade.php
    
    commit 462f0de8ded327921ff911842c1500460dac3629
    Author: caffi <caffi.senna@gmail.com>
    Date:   Fri Nov 5 00:09:47 2021 +0900
    
        Update home.blade.php
    
    commit c7c0bde31a0d246ebe1fa53a59c14641ac7f0476
    Author: caffi <caffi.senna@gmail.com>
    Date:   Fri Nov 5 00:03:27 2021 +0900
    
        Update home.blade.php
    
    commit 9d3e3882a30193619b2b862445b66c4bf2582794
    Author: caffi <caffi.senna@gmail.com>
    Date:   Thu Nov 4 21:50:53 2021 +0900
    
        Update table.blade.php
    
    commit eb1d7043d693ff40e04c2ef4281f8bb8f6e67f46
    Author: caffi <caffi.senna@gmail.com>
    Date:   Sun Oct 31 00:07:59 2021 +0900
    
        申込書リンク修正
    
    commit e6c14e45c23c8c943f5fa7f76070281495e8a149
    Author: caffi <caffi.senna@gmail.com>
    Date:   Sat Oct 30 15:37:52 2021 +0900
    
        Update menu.blade.php
    
    commit 3b814da3d1b5d0d3297641abe05de9f1170888ab
    Author: caffi <caffi.senna@gmail.com>
    Date:   Sat Oct 30 14:53:16 2021 +0900
    
        計画に参加形態を表示するように

--- resources/views/result_uploads/index.blade.php
+++ resources/views/result_uploads/index.blade.php
@@ -31,6 +31,7 @@
             <li>合計タイム: {{ $resultUploads->hms }}</li>
             <li>総距離: {{ $resultUploads->distance_sum }}<span class="uk-text-small">km</span></li>
         </ul>
+        <p class="uk-text-warning">大会の公式記録が確定するまではTATTAアプリでの歩行記録は削除しないようにしてください。</p>
 
         <div class="card">
             <div class="card-body p-0">
