28dcbba80e3534e67d31c40858d4dc9b0652c11c
caffi <caffi.senna@gmail.com>
2022-09-23 22:18:52 +0900

    feature#13
    close #13

--- config/app.php
+++ config/app.php
@@ -175,6 +175,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class, // dompdf
 
     ],
 
@@ -230,6 +231,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Flash' => Laracasts\Flash\Flash::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class, // dompdf
     ],
 
 ];
