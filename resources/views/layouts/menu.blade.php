6e502d0e94a30df7329f1e78d9dacecf7994ed8b
caffi <caffi.senna@gmail.com>
2022-10-01 22:03:20 +0900

    feature#34
    close #34

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -90,6 +90,12 @@
             <p>バディリスト</p>
         </a>
     </li>
+    <li class="nav-item">
+        <a href="{{ url('/admin/fee_check') }}"
+           class="nav-link {{ Request::is('admin/fee_check*') ? 'active' : '' }}">
+            <p>参加費チェック</p>
+        </a>
+    </li>
 
 
 
