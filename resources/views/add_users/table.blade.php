837227c55e628cc3134c50bd219bc2c6b29e80bd
caffi <caffi.senna@gmail.com>
2023-10-09 16:43:45 +0900

    マスターアカウント非表示

--- resources/views/add_users/table.blade.php
+++ resources/views/add_users/table.blade.php
@@ -12,29 +12,31 @@
             </thead>
             <tbody>
                 @foreach ($users as $users)
-                    <tr>
-                        <td>{{ $users->name }}</td>
-                        <td>{{ $users->email }}</td>
-                        <td>{{ $users->role }}</td>
-                        <td>{{ $users->district }}</td>
-                        <td style="width: 120px">
-                            {!! Form::open(['route' => ['add_users.destroy', $users->id], 'method' => 'delete']) !!}
-                            <div class='btn-group'>
-                                <a href="{{ route('add_users.show', [$users->id]) }}" class='btn btn-default btn-xs'>
-                                    <i class="far fa-eye"></i>
-                                </a>
-                                <a href="{{ route('add_users.edit', [$users->id]) }}" class='btn btn-default btn-xs'>
-                                    <i class="far fa-edit"></i>
-                                </a>
-                                {!! Form::button('<i class="far fa-trash-alt"></i>', [
-                                    'type' => 'submit',
-                                    'class' => 'btn btn-danger btn-xs',
-                                    'onclick' => "return confirm('Are you sure?')",
-                                ]) !!}
-                            </div>
-                            {!! Form::close() !!}
-                        </td>
-                    </tr>
+                    @unless ($users->email == 'caffi.senna@gmail.com')
+                        <tr>
+                            <td>{{ $users->name }}</td>
+                            <td>{{ $users->email }}</td>
+                            <td>{{ $users->role }}</td>
+                            <td>{{ $users->district }}</td>
+                            <td style="width: 120px">
+                                {!! Form::open(['route' => ['add_users.destroy', $users->id], 'method' => 'delete']) !!}
+                                <div class='btn-group'>
+                                    <a href="{{ route('add_users.show', [$users->id]) }}" class='btn btn-default btn-xs'>
+                                        <i class="far fa-eye"></i>
+                                    </a>
+                                    <a href="{{ route('add_users.edit', [$users->id]) }}" class='btn btn-default btn-xs'>
+                                        <i class="far fa-edit"></i>
+                                    </a>
+                                    {!! Form::button('<i class="far fa-trash-alt"></i>', [
+                                        'type' => 'submit',
+                                        'class' => 'btn btn-danger btn-xs',
+                                        'onclick' => "return confirm('Are you sure?')",
+                                    ]) !!}
+                                </div>
+                                {!! Form::close() !!}
+                            </td>
+                        </tr>
+                    @endunless
                 @endforeach
             </tbody>
         </table>
