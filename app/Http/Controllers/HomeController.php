dd96a393291f7af99f3a4f426c3e5a08d941e43f
caffi <caffi.senna@gmail.com>
2023-10-15 10:26:33 +0900

    集計を表示する

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\AdminConfig;
 use App\Models\User;
+use App\Models\entryForm;
+use Illuminate\Support\Facades\DB;
+use Auth;
+use App\Http\Controllers\commiEntryFormController;
 
 class HomeController extends Controller
 {
@@ -30,6 +34,26 @@ class HomeController extends Controller
         // 健康調査の判定フラグ用にユーザーデータを取る
         $user = User::where('id', Auth()->id())->with('elearning')->first();
 
-        return view('home', compact(['configs','user']));
+        // $district_counts = EntryForm::select('district', DB::raw('count(*) as count'))
+        //     ->whereNull('deleted_at')
+        //     ->groupBy('district')
+        //     ->orderBy('district', 'desc')
+        //     ->get();
+        $districtCounts = EntryForm::select('district')
+            ->selectRaw('COUNT(*) as total_count')
+            ->selectRaw('SUM(CASE WHEN generation = "現役" THEN 1 ELSE 0 END) as active_count')
+            ->selectRaw('SUM(CASE WHEN generation = "オーバーエイジ" THEN 1 ELSE 0 END) as over_age_count')
+            ->selectRaw('SUM(CASE WHEN gender = "男" THEN 1 ELSE 0 END) as male_count')
+            ->selectRaw('SUM(CASE WHEN gender = "女" THEN 1 ELSE 0 END) as female_count')
+            ->whereNull('deleted_at')
+            ->groupBy('district')
+            ->orderBy('district', 'desc')
+            ->get();
+
+        // if (Auth::user()->is_commi) {
+            // return redirect()->roure('commiEntryFormController.index');
+        // } else {
+            return view('home', compact(['configs', 'user', 'districtCounts']));
+        // }
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,10 @@
+{{-- {{ dd($district_counts) }} --}}
 @extends('layouts.app')
 @section('content')
     <div class="container">
         <div class="row">
             @if (isset(Auth::user()->email_verified_at))
-                @unless(Auth::user()->is_admin || Auth::user()->is_commi || Auth::user()->is_staff)
+                @unless (Auth::user()->is_admin || Auth::user()->is_commi || Auth::user()->is_staff)
                     <div class="card" style="width:100%;">
                         <div class="card-header">
                             <h4>各種情報</h4>
@@ -24,7 +25,6 @@
                         </div>
                     </div>
                 @endunless
-
             @else
                 <div class="card">
                     <h5 class="card-header text-danger">メール認証が完了していません</h5>
@@ -47,15 +47,73 @@
 
             {{-- 管理者のみ表示 --}}
             @if (Auth::user()->is_admin)
-                <div class="uk-card uk-card-secondary uk-card-body">
+                {{-- <div class="uk-card uk-card-secondary uk-card-body">
                     <h3 class="uk-card-title">注意</h3>
                     <p class="uk-text-danger">管理者としてログイン中です。<br>
                         データの削除、変更も可能なため操作にはご注意ください。<br>
                         表示量が多いためPCでの閲覧を推奨します。<br>
                         情報の取り扱いには充分ご注意ください。
                     </p>
-                </div>
+                </div> --}}
+                <h3>地区別内訳</h3>
+                <table class="uk-table uk-table-small uk-table-divider">
+                    <thead>
+                        <tr>
+                            <th>地区</th>
+                            <th>合計人数</th>
+                            <th>現役</th>
+                            <th>OA</th>
+                            <th>男</th>
+                            <th>女</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($districtCounts as $districtCount)
+                            <tr>
+                                <td>{{ $districtCount->district }}</td>
+                                <td>{{ $districtCount->total_count }}</td>
+                                <td>{{ $districtCount->active_count }}</td>
+                                <td>{{ $districtCount->over_age_count }}</td>
+                                <td>{{ $districtCount->male_count }}</td>
+                                <td>{{ $districtCount->female_count }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             @endif
-        </div>
+            @if (Auth::user()->is_commi)
+                <h3>{{ Auth::user()->is_commi }}地区別内訳</h3>
+
+                <table class="uk-table uk-table-small uk-table-divider">
+                    <thead>
+                        <tr>
+                            <th>地区</th>
+                            <th>合計人数</th>
+                            <th>現役</th>
+                            <th>OA</th>
+                            <th>男</th>
+                            <th>女</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($districtCounts as $districtCount)
+                            @if ($districtCount->district == Auth::user()->is_commi)
+                                <tr>
+                                    <td>{{ $districtCount->district }}</td>
+                                    <td>{{ $districtCount->total_count }}</td>
+                                    <td>{{ $districtCount->active_count }}</td>
+                                    <td>{{ $districtCount->over_age_count }}</td>
+                                    <td>{{ $districtCount->male_count }}</td>
+                                    <td>{{ $districtCount->female_count }}</td>
+                                </tr>
+                            @endif
+                        @endforeach
+                    </tbody>
+                </table>
+                <a href="{{ route('entries.index') }}" class="uk-button uk-button-primary">
+                    地区参加者一覧を表示
+                </a>
+            @endif
+    </div>
     </div>
 @endsection
