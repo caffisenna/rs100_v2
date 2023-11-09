4d4a3a213dcdeb18b68df6019d686629bda966c6
caffi <caffi.senna@gmail.com>
2023-11-10 00:11:45 +0900

    Eラン終了

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -122,6 +122,7 @@
                 <div class="uk-card uk-card-primary uk-card-body">
                     <h5 class="uk-card-title"><span uk-icon="icon:info; ratio:1"></span>NEWS</h5>
                     <ul class="uk-list">
+                        <li>2023/11/10 Eラーニングの受講を終了しました</li>
                         <li>2023/11/01 <a href="{{ url('/car_registrations') }}">駐車許可証申請フォーム</a>をリリースしました</li>
                         <li>2023/11/01 Eラーニングの受講が可能になりました</li>
                         <li>2023/10/31 参加IDカードのダウンロードが可能になりました</li>
