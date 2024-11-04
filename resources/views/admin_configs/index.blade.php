55ebdb2c626f6036b88dac0829abf47e4e66b576
caffi <caffi.senna@gmail.com>
2024-11-04 14:42:06 +0900

    small fix

--- resources/views/admin_configs/index.blade.php
+++ resources/views/admin_configs/index.blade.php
@@ -5,7 +5,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Admin Configs</h1>
+                    <h1>システム設定</h1>
                 </div>
                 <div class="col-sm-6">
                 </div>
